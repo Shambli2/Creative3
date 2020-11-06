406ea97e8caec26c4f1479583d22433bd48f3eb6
Sarah Hamblin <hamblinsarah14@gmail.com>
2020-11-05 18:40:33 -0700

    finished

---
+++ src/mock-data.js
@@ -0,0 +1,261 @@
+let mock = [{
+  id: 1,
+  name: "Studs",
+  color: "Black",
+  price: "$3",
+  image: 'product1.jpg'
+},
+{
+  id: 2,
+  name: "Studs",
+  color: "Pink",
+  price: "$3",
+  image: 'product2.jpg'
+},
+{
+  id: 3,
+  name: "Studs",
+  color: "Multi-colored",
+  price: "$3",
+  image: 'product3.jpg'
+},
+{
+  id: 4,
+  name: "Studs",
+  color: "Multi-colored",
+  price: "$3",
+  image: 'product4.jpg'
+},
+{
+  id: 5,
+  name: "Studs",
+  color: "Blue",
+  price: "$3",
+  image: 'product5.jpg'
+},
+{
+  id: 6,
+  name: "Studs",
+  color: "Pink",
+  price: "$3",
+  image: 'product6.jpg'
+},
+{
+  id: 7,
+  name: "Studs",
+  color: "Blue",
+  price: "$3",
+  image: 'product7.jpg'
+},
+{
+  id: 8,
+  name: "Tiered/Dangles",
+  color: "Black",
+  price: "$7",
+  image: 'product8.jpg'
+},
+{
+  id: 9,
+  name: "Tiered/Dangles",
+  color: "Blue",
+  price: "$7",
+  image: 'product9.jpg'
+},
+{
+  id: 10,
+  name: "Tiered/Dangles",
+  color: "Black",
+  price: "$7",
+  image: 'product10.jpg'
+},
+{
+  id: 11,
+  name: "Tiered/Dangles",
+  color: "Tan",
+  price: "$7",
+  image: 'product11.jpg'
+},
+{
+  id: 12,
+  name: "Tiered/Dangles",
+  color: "Red",
+  price: "$7",
+  image: 'product12.jpg'
+},
+{
+  id: 13,
+  name: "Tiered/Dangles",
+  color: "Pink",
+  price: "$7",
+  image: 'product13.jpg'
+},
+{
+  id: 14,
+  name: "Tiered/Dangles",
+  color: "Tan",
+  price: "$7",
+  image: 'product14.jpg'
+},
+{
+  id: 15,
+  name: "Tiered/Dangles",
+  color: "Pink",
+  price: "$7",
+  image: 'product15.jpg'
+},
+{
+  id: 16,
+  name: "Tiered/Dangles",
+  color: "Black",
+  price: "$7",
+  image: 'product16.jpg'
+},
+{
+  id: 17,
+  name: "Tiered/Dangles",
+  color: "Red",
+  price: "$6",
+  image: 'product17.jpg'
+},
+{
+  id: 18,
+  name: "Tiered/Dangles",
+  color: "Pink",
+  price: "$6",
+  image: 'product18.jpg'
+},
+{
+  id: 19,
+  name: "Tiered/Dangles",
+  color: "Black",
+  price: "$6",
+  image: 'product19.jpg'
+},
+{
+  id: 20,
+  name: "Tiered/Dangles",
+  color: "Black",
+  price: "$6",
+  image: 'product20.jpg'
+},
+{
+  id: 21,
+  name: "Tiered/Dangles",
+  color: "Pink",
+  price: "$6",
+  image: 'product21.jpg'
+},
+{
+  id: 22,
+  name: "Tiered/Dangles",
+  color: "Orange",
+  price: "$6",
+  image: 'product22.jpg'
+},
+{
+  id: 23,
+  name: "Tiered/Dangles",
+  color: "Pink",
+  price: "$6",
+  image: 'product23.jpg'
+},
+{
+  id: 24,
+  name: "Tiered/Dangles",
+  color: "Pink",
+  price: "$6",
+  image: 'product24.jpg'
+},
+{
+  id: 25,
+  name: "Tiered/Dangles",
+  color: "Blue",
+  price: "$6",
+  image: 'product25.jpg'
+},
+{
+  id: 26,
+  name: "Tiered/Dangles",
+  color: "BLue",
+  price: "$6",
+  image: 'product26.jpg'
+},
+{
+  id: 27,
+  name: "Tiered/Dangles",
+  color: "Pink",
+  price: "$6",
+  image: 'product27.jpg'
+},
+{
+  id: 28,
+  name: "Tiered/Dangles",
+  color: "Pink",
+  price: "$6",
+  image: 'product28.jpg'
+},
+{
+  id: 29,
+  name: "Tiered/Dangles",
+  color: "Red",
+  price: "$6",
+  image: 'product29.jpg'
+},
+{
+  id: 30,
+  name: "Studs",
+  color: "Red",
+  price: "$3",
+  image: 'product30.jpg'
+},
+{
+  id: 31,
+  name: "Studs",
+  color: "Tan",
+  price: "$3",
+  image: 'product31.jpg'
+},
+{
+  id: 32,
+  name: "Studs",
+  color: "Purple",
+  price: "$3",
+  image: 'product32.jpg'
+},
+{
+  id: 33,
+  name: "Studs",
+  color: "Pink",
+  price: "$3",
+  image: 'product33.jpg'
+},
+{
+  id: 34,
+  name: "Studs",
+  color: "Pink",
+  price: "$3",
+  image: 'product34.jpg'
+},
+{
+  id: 35,
+  name: "Studs",
+  color: "Green",
+  price: "$3",
+  image: 'product35.jpg'
+},
+{
+  id: 36,
+  name: "Tiered/Dangles",
+  color: "Pink",
+  price: "$4",
+  image: 'product36.jpg'
+},
+{
+  id: 37,
+  name: "Tiered/Dangles",
+  color: "Multi-colored",
+  price: "$4",
+  image: 'product37.jpg'
+}
+]
+export default mock;
